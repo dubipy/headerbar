014178699fe961a56dec393335a15198eadfc9d9
dubipy <qnfrha999@naver.com>
2022-08-29 20:48:44 +0900

    headerbar

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+// import { createGlobalStyle } from 'styled-components';
+import Header from './Header';
+
+
+function App() {
+  return (
+    <div>
+      {/* <GlobalStyles/> */}
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
